3f1b48ab41abe9dc31298fbaaef96675556b0266
sbohmann <github@yeoman.at>
2024-07-23 19:34:55 +0200

    substitutions

--- roundfunction.js
+++ roundfunction.js
@@ -1,3 +1,5 @@
+import {substitutions} from "./substitutions.js";
+
 export function roundFunction(inputState, key) {
     if (inputState.length !== 4) {
         throw new RangeError(`expected state length 4, provided: ${inputState.length}`)
@@ -14,14 +16,13 @@ function subRoundKey(key, index) {
     let shift  = (index % 2  === 0)
         ? 4
         : 0
-    return (key[byteIndex] >> shift) & 0x0f;
+    return (key[byteIndex] >>> shift) & 0x0f;
 }
 
 // half = 16 bits (half of a1)
 // 0xxx ror by 1-9
 // 10xx apply one out of 4 sets of 8 fixed or provided substitutions
-// 11xx shuffle using one out of 4 fixed or provided shuffles
-
+// 11xx shuffle using one out of 2 fixed or provided shuffles
 function performSubRound(state, key) {
     if ((key & 0b1000) === 0) {
         // 0xxx
@@ -29,10 +30,36 @@ function performSubRound(state, key) {
         return rotateRight(state, rotationAmount)
     } else if ((key & 0b1100) === 0b1000) {
         // 10xx
-        return undefined // TODO
+        let substitutionIndex = key & 0b0011
+        return substitute(state, substitutionIndex)
+    } else if ((key & 0b1110) === 0b1100) {
+        // 10xx
+        // TODO shuffle
+    } else {
+        throw new RangeError("Programming error. This block should be unreachable")
     }
 }
 
 function rotateRight(value, n) {
-    return (value >> n) | (value << (4 - n))
+    return (value >>> n) | (value << (4 - n))
+}
+
+function substitute(value, substitutionIndex) {
+    let substitution = substitutions[substitutionIndex]
+    let result = 0
+    for (let nibbleIndex = 0; nibbleIndex < 4; ++nibbleIndex) {
+        let originalNibbleValue = value & 0xf0000000
+        value <<= 4
+        result <<= 4
+        result |= substituteNibble(originalNibbleValue, substitution, nibbleIndex)
+    }
+    return result
+}
+
+function substituteNibble(nibble, substitution, nibbleIndex) {
+    if (nibble < 0 || nibble >= 16 || !Number.isInteger(nibble)) {
+        throw new RangeError(`Not a valid nibble value: ${nibble}`)
+    }
+    let substitutionForPart = substitution[nibbleIndex];
+    return substitutionForPart[nibble]
 }
