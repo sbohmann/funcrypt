e8bccfd8897af4df3e36c2623675fe8fe6ec30ea
sbohmann <github@yeoman.at>
2024-07-24 17:09:33 +0200

    Avalanche demonstration script fix

--- avalanche.sh
+++ avalanche.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 echo -n 'hsojdluy' | node funcrypt -key 'd982ba6303097ffab5d269c85e85fbfc56b63b75d2e20c0c0a415495d2a5cf63' | hexdump
-echo -n 'hsojdluy' | node funcrypt -key 'd882ba6303097ffab5d269c85e85fbfc56b63b75d2e20c0c0a415495d2a5cf62' | hexdump
+echo -n 'hsojdluy' | node funcrypt -key 'd982ba6303097ffab5d269c85e85fbfc56b63b75d2e20c0c0a415495d2a5cf62' | hexdump
 echo -n 'hsokdluy' | node funcrypt -key 'd982ba6303097ffab5d269c85e85fbfc56b63b75d2e20c0c0a415495d2a5cf62' | hexdump
 echo -n 'hsojdluy' | node funcrypt -key 'd882ba6303097ffab5d269c85e85fbfc56b63b75d2e20c0c0a415495d2a5cf63' | hexdump
 echo -n 'hsojdluy' | node funcrypt -key 'd882ba6303097ffab5d269c85e85fbfc56b63b75d2e20c0c0a415495d2a5cf62' | hexdump
