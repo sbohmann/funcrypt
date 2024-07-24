335e4dec1d19a1cec139025094c1f1488a060ee0
sbohmann <github@yeoman.at>
2024-07-24 16:32:03 +0200

    Added main to package.json, and an avalanche demonstration script

--- shufflekey.js
+++ shufflekey.js
@@ -1,9 +1,10 @@
 export function shuffleKey(key) {
+    let originalKey = Array.from(key)
     if (key.length !== 32) {
         throw new RangeError(`Key length ${key.length} != 32`)
     }
     for (let index = 0; index < 16; ++index) {
-        key[index] ^= key[index + 16]
-        key[index + 16] ^= key[16 - index]
+        key[index] ^= originalKey[index + 16]
+        key[index + 16] ^= originalKey[15 - index]
     }
 }
